56dc2624c7a1d9461360313df92f5c0a5f0c7256
Konstantin Gazha <kgazha@gmail.com>
2019-03-10 22:18:12 +0500

    Change department display

--- phonebook/core/views.py
+++ phonebook/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.views import View
 from .models import Organization, Department, OrgDepAssociation, Position, Employee, Contact, ContactType
+from collections import defaultdict
 
 
 def get_contacts(employee):
@@ -14,31 +15,39 @@ def get_contacts(employee):
 class Phonebook:
     def __init__(self):
         self.department_key = Department._meta.verbose_name.title()
+        self.position_key = Position._meta.verbose_name.title()
         self.employee_key = Employee._meta.verbose_name.title()
+        self.employees_key = Employee._meta.verbose_name_plural.title()
         self.header = self.__update_header()
         self.content = []
 
     def __update_header(self):
         contact_types = [value.__str__()
                          for value in ContactType.objects.filter(phonebook_display=True).order_by('phonebook_order')]
-        return [self.department_key, self.employee_key] + contact_types
+        return [self.position_key, self.employee_key] + contact_types
 
     def get_phonebook(self, organization):
         org_deps = OrgDepAssociation.objects.filter(organization=organization).order_by('phonebook_order')
         for org_dep in org_deps:
             department = Department.objects.get(id=org_dep.department.id)
             employees = Employee.objects.filter(org_dep_association=org_dep).order_by('phonebook_order')
+            data = defaultdict(list)
+            data[self.department_key] = department.name
             for employee in employees:
                 contacts = get_contacts(employee)
-                data = {self.department_key: department.name,
-                        self.employee_key: employee}
+                employee_dict = defaultdict(dict)
+                employee_dict.update({self.position_key: employee.position})
+                employee_dict.update({self.employee_key: employee})
                 for key in self.header:
                     if key in contacts.keys():
-                        data.update({key: contacts[key]})
-                    elif key not in data.keys():
-                        data.update({key: None})
-                self.content.append(data)
-        return {'phonebook_header': self.header, 'phonebook_content': self.content}
+                        employee_dict.update({key: contacts[key]})
+                    elif key not in employee_dict.keys():
+                        employee_dict.update({key: ''})
+                data[self.employees_key].append(dict(employee_dict))
+            self.content.append(dict(data))
+        return {'phonebook_header': self.header, 'phonebook_content': self.content,
+                'department_key': self.department_key, 'employee_key': self.employee_key,
+                'position_key': self.position_key}
 
 
 class PhonebookView(View):
@@ -47,7 +56,11 @@ class PhonebookView(View):
         context = {'organizations': organizations}
         if 'slug' in kwargs:
             organization = Organization.objects.filter(slug=kwargs['slug'])
+            if organization:
+                organization = organization[0]
+        else:
+            organization = Organization.objects.all()[0]
         if organization:
-            phonebook = Phonebook().get_phonebook(organization[0])
-            context.update({'phonebook': phonebook})
+            phonebook = Phonebook().get_phonebook(organization)
+            context.update(phonebook)
         return render(request, 'phonebook.html', context)
